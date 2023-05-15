893338bc70a862deb0b10c763572a1b5163a9659
jeongjwon <zlzlsksk111@gmail.com>
2023-05-15 13:38:37 +0900

    app.css update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+import ProductsList from "./pages/ProductsList";
+import Bookmark from "./pages/Bookmark";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/products/list" element={<ProductsList />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ProductsList.js
@@ -0,0 +1,4 @@
+const ProductsList = () => {
+
+}
+export default ProductsList;
