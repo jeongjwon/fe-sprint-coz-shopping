dd34935362671d9ce870162df4def5e25ae6d4b2
jeongjwon <zlzlsksk111@gmail.com>
2023-05-15 12:00:34 +0900

    header, footer 완성

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,17 @@
-const Footer = () => {
+import styled from "styled-components";
 
+export const FooterContainer = styled.footer`
+    text-align: center;
+    color: #888888;
+    font-size: 1.2rem;
+    font-weight: 400;
+`;
+const Footer = () => {
+    return(
+        <FooterContainer>
+            <div>개인정보 처리방침 | 이용 약관</div>
+            <div>All rights reserved @ Codestates</div>
+        </FooterContainer>
+    )
 }
 export default Footer;
