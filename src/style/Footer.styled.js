f11c5ae05f87a9dccb36c31b5327b55ac19004e5
jeongjwon <zlzlsksk111@gmail.com>
2023-05-17 17:42:43 +0900

    💄 border-top 추가

--- src/style/Footer.styled.js
+++ src/style/Footer.styled.js
@@ -5,4 +5,6 @@ export const FooterContainer = styled.footer`
     color: #888888;
     font-size: 1.2rem;
     font-weight: 400;
+    padding: 1rem 0 ;
+    border-top : 1px solid rgba(0, 0, 0, 0.1);
 `;
