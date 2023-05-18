67e50db1c5543e07eda4d48b1f2d94a748eb4c73
jeongjwon <zlzlsksk111@gmail.com>
2023-05-18 19:08:43 +0900

    ✨ new. 새 컴포넌트 생성

---
+++ src/components/Item.js
@@ -0,0 +1,91 @@
+import * as P from "../style/Item.styled";
+import { useSelector, useDispatch } from "react-redux";
+import {modalActions} from "../store/modal";
+import { HiOutlineStar } from "react-icons/hi";
+
+const Item = ({ item }) => {
+    if(!item) return null;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+   const dispatch = useDispatch();
+
+    const handleOpenModal =() => {
+        dispatch(modalActions.open());
+        dispatch(modalActions.showModal(item));
+    }
+   
+  const showCard = (item) => {
+    switch (item.type) {
+      case "Product":
+        return (
+          <>
+            <P.ItemDesc_Up>
+              <strong>{item.title}</strong>
+              <div className="sale">{item.discountPercentage}%</div>
+            </P.ItemDesc_Up>
+            <P.ItemDesc_Down>
+              <div className="price">
+                {Number(item.price).toLocaleString("ko-KR")}원
+              </div>
+            </P.ItemDesc_Down>
+          </>
+        );
+      case "Category":
+        return (
+          <>
+            <P.ItemDesc_Up>
+              <strong># {item.title}</strong>
+            </P.ItemDesc_Up>
+            <P.ItemDesc_Down></P.ItemDesc_Down>
+          </>
+        );
+      case "Exhibition":
+        return (
+          <>
+            <P.ItemDesc_Up>
+              <strong>{item.title}</strong>
+            </P.ItemDesc_Up>
+            <P.ItemDesc_Down>
+              <div className="detail-desc">{item.sub_title} </div>
+            </P.ItemDesc_Down>
+          </>
+        );
+      case "Brand":
+        return (
+          <>
+            <P.ItemDesc_Up>
+              <strong>{item.brand_name}</strong>
+              <div className="follower_up">관심고객수</div>
+            </P.ItemDesc_Up>
+            <P.ItemDesc_Down>
+              <div className="follower_down">
+                {Number(item.follower).toLocaleString("ko-KR")}
+              </div>
+            </P.ItemDesc_Down>
+          </>
+        );
+      default:
+        return <></>;
+    }
+  };
+  return (
+    <P.ItemWrapper>
+      <P.ItemImage
+      onClick={handleOpenModal}
+        className="item-img"
+        style={{
+          background: `url(${item.image_url || item.brand_image_url})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <P.BookmarkBtn className="item-bookmark" type="button">
+          <HiOutlineStar size={35} />
+          {/* { isMarked ? <HiOutlineStar size={35} style={{fill:"#ffd361"}} /> : <HiOutlineStar size={35} /> } */}
+        </P.BookmarkBtn>
+      </P.ItemImage>
+
+      <P.ItemDesc>{showCard(item)}</P.ItemDesc>
+    </P.ItemWrapper>
+  );
+};
+export default Item;
