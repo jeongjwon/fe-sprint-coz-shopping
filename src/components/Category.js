590ea747f6346a208cb53755dcccfa0592b3b8e0
jeongjwon <zlzlsksk111@gmail.com>
2023-05-18 14:22:58 +0900

    ♻️ category 컴포넌트 리팩토링

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,36 +6,31 @@ import category from "../assets/productsNav/category.png";
 import exhibition from "../assets/productsNav/exhibition.png";
 import brand from "../assets/productsNav/brand.png";
 
-import * as C from "../style/Category.styled"
+import * as C from "../style/Category.styled";
 
-const ProductsNav = ({setIndex}) => {
-    const [images, setImages] = useState([]);
-    const titles = ["전체", "상품", "카테고리", "기획전", "브랜드"]
+const Category = ({ setIndex }) => {
+  const categoryLists = [
+    { title: "전체", type: "All", img_url: all },
+    { title: "상품", type: "Product", img_url: product },
+    { title: "카테고리", type: "Category", img_url: category },
+    { title: "기획전", type: "Exhibition", img_url: exhibition },
+    { title: "브랜드", type: "Brand", img_url: brand },
+  ];
 
+  return (
+    <C.NavContainer>
+      {categoryLists.map((list, idx) => {
+        return (
+          <C.NavList key={idx} onClick={() => setIndex(idx)}>
+            <div className="nav-box">
+              <img src={list.img_url} alt={list.img_url} />
+              <span>{list.title}</span>
+            </div>
+          </C.NavList>
+        );
+      })}
+    </C.NavContainer>
+  );
+};
 
-    useEffect(() => {
-        setImages([all,product, category, exhibition, brand]);
-    },[]);
-
-    return(
-        <C.NavContainer>
-            {images.map((image,idx) => {
-                return(
-                    <C.NavList key={idx} onClick={()=>setIndex(idx)}>
-                        <div className="nav-box">
-                            <img src={image} alt={image} />
-                            <span>{titles[idx]}</span>
-                        </div>
-                        
-                    </C.NavList>
-                );
-                
-            })}
-            
-            
-           
-        </C.NavContainer>
-    )
-}
-
-export default ProductsNav;
+export default Category;
