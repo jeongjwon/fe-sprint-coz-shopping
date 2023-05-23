365f8753a8047c0b58d8e8f82dc8a9f7b29d6368
jeongjwon <zlzlsksk111@gmail.com>
2023-05-23 10:59:35 +0900

    ♻️ Refactor: 전역변수 설정

---
+++ src/constants/title.js
@@ -0,0 +1,4 @@
+export const title = {
+    PRODUCT_LIST: '상품 리스트',
+    BOOKMARK_LIST: '북마크 리스트'
+}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,14 @@
 import * as M from "../style/Main.styled";
 import ProductList from "../components/ProductList";
 import BookmarkList from "../components/BookmarkList";
+import { title } from "../constants/title";
 const Main = () => {
 
     return(
         <M.MainContainer>
-            <ProductList />
-            <BookmarkList />
+            <ProductList title={title.PRODUCT_LIST} />
+            <ProductList title={title.BOOKMARK_LIST}/>
+            {/* <BookmarkList /> */}
         </M.MainContainer>
     )
 }
