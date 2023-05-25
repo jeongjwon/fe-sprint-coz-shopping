f9ab6953254125aee27fb56a4f348e04db53cf14
jeongjwon <zlzlsksk111@gmail.com>
2023-05-25 15:49:34 +0900

    폴더 수정

--- src/stories/Bookmark.stories.js
+++ src/stories/Bookmark.stories.js
@@ -1,11 +1,8 @@
 import { Bookmark } from "./Bookmark";
 
 export default {
-    title: 'Atoms/Bookmark', // 스토리 분류 및 컴포넌트 이름
+    title: 'Atoms/BookmarkButton', // 스토리 분류 및 컴포넌트 이름
     component: Bookmark, // 테스트할 컴포넌트(Icon)
 };
 
-export const Icon = (args) => <Bookmark {...args} />
-Icon.args={
-    
-}
+export const BookmarkButton = (args) => <Bookmark {...args} />
